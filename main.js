c0678dea79be20986c514cde3c40031f1ce79382
Janne111 <ja.kroemer@hbk-bs.de>
2026-07-04 11:42:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -50,3 +50,29 @@ audio2.addEventListener('pause', updateButtonColor2);
 audio2.addEventListener('ended', updateButtonColor2);
 
 updateButtonColor2();
+
+const audio3 = document.getElementById('weg-audio');
+const playButton3 = document.getElementById('playone');
+
+function updateButtonColor3() {
+    if (audio3.paused) {
+        playButton3.style.borderColor = 'transparent transparent transparent white';
+    } else {
+        playButton3.style.borderColor = 'transparent transparent transparent rgb(39, 39, 39)';
+        playButton3.style.transition = '0.5s'
+    }
+}
+
+playButton3.addEventListener('click', () => {
+    if (audio3.paused) {
+        audio3.play();
+    } else {
+        audio3.pause();
+    }
+});
+
+audio3.addEventListener('play', updateButtonColor3);
+audio3.addEventListener('pause', updateButtonColor3);
+audio3.addEventListener('ended', updateButtonColor3);
+
+updateButtonColor3();
